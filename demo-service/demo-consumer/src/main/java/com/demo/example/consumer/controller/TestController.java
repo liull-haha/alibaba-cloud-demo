12552cea9b7e2cabcbc9f9f6d931aa355daf0fd7
liull <17756545127@163.com>
2021-03-30 11:38:35 +0800

    初始化

---
+++ demo-service/demo-consumer/src/main/java/com/demo/example/consumer/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.demo.example.consumer.controller;
+
+import com.demo.example.consumer.openFeign.EchoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liull
+ * @description
+ * @date 2021/3/24
+ */
+@RestController
+public class TestController {
+    @Autowired
+    private EchoService echoService;
+
+
+
+    @RequestMapping(value = "/echo-feign/{str}", method = RequestMethod.GET)
+    public String feign(@PathVariable String str) {
+        return echoService.echo(str);
+    }
+
+}
