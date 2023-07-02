3fee947ad693ff7921f59ed84dfe13359baec36a
liull <liull@asiainfo.com>
2023-07-02 21:36:29 +0800

    新增模块

---
+++ demo-pattern/src/main/java/com/example/demopattern/DemoPatternApplication.java
@@ -0,0 +1,13 @@
+package com.example.demopattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPatternApplication.class, args);
+    }
+
+}
